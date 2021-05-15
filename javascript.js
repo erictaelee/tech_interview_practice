80dd527684145a1dc0e706f697fc0f6921c5dd06
Eric <lee.erictae@gmail.com>
2021-05-15 12:50:06 -0500

    resolve the greatest number

--- javascript.js
+++ javascript.js
@@ -4,10 +4,24 @@
 
 var array = [5, 17, -4, 20, 12];
 var i = 0;
+var greatestNumber = Math.min.apply(null, array);
+
+while (i < array.length) {
+  if (greatestNumber < array[i]) {
+    greatestNumber = array[i];
+  }
+  i++;
+}
+console.log(greatestNumber)
+
+// OR
+
+var array = [5, 17, -4, 20, 12];
+var i = 0;
 var greatestNumber = array[0];
 
 while (i < array.length) {
-  if (array[i] > array[i + 1]) {
+  if (greatestNumber < array[i]) {
     greatestNumber = array[i];
   }
   i++;
