67247df777b16d016b59b375f47281c53a6d9079
Eric <lee.erictae@gmail.com>
2021-05-17 22:48:35 -0500

    solve with map loop in ruby

--- double.rb
+++ double.rb
@@ -4,14 +4,26 @@
 # Output: [8, 4, 10, 198, -8]
 
 
+# def double_array(array)
+#   i = 0
+#   double_array = []
+#   while i < array.length
+#     double_array << array[i] * 2
+#     i += 1
+#   end
+#   return double_array
+# end
+
+# p double_array([4, 2, 5, 99, -4])
+
+
+
 def double_array(array)
-  i = 0
-  double_array = []
-  while i < array.length
-    double_array << array[i] * 2
-    i += 1
+# array = [4, 2, 5, 99, -4]
+  double_array = array.map do |array|
+    array * 2 
   end
+  # p double_array
   return double_array
 end
-
 p double_array([4, 2, 5, 99, -4])
