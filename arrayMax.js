4e6748c09b3b97d4ad9cec7986ff7ea113a079b3
Eric <lee.erictae@gmail.com>
2021-05-20 23:32:43 -0500

    used for loop

--- arrayMax.js
+++ arrayMax.js
@@ -3,14 +3,28 @@
 
 
 
+// function greatestNum(array) {
+//   var i = 0;
+//   var greatestNum = array[0];
+//   while (i < array.length) {
+//     if (greatestNum < array[i]) {
+//       greatestNum = array[i]
+//     }
+//     i++;
+//   }
+
+//   return greatestNum;
+// }
+
+// console.log(greatestNum([50, 5, 17, -4, 20, 12,]));
+
+
 function greatestNum(array) {
-  var i = 0;
-  var greatestNum = array[0];
-  while (i < array.length) {
+  greatestNum = array[0];
+  for (let i = 0; i < array.length; i++) {
     if (greatestNum < array[i]) {
-      greatestNum = array[i]
+      greatestNum = array[i];
     }
-    i++;
   }
 
   return greatestNum;
