d5ad8afba5a25ec88860a5178a614b20dbdda231
Eric <lee.erictae@gmail.com>
2021-05-23 10:58:50 -0500

    solve reverse string in js

---
+++ reverseString.js
@@ -0,0 +1,26 @@
+// Return the reverse of a given string.
+
+// Input: “abcde”
+// Output: “edcba”
+
+
+
+//split string into an array
+//set index to the last array value (i = string.length - 1)
+//use for loop
+//create a new empty array
+//push index to the new array
+//decrement index
+//return reverse.join()
+
+
+function reverse(string) {
+  let split = string.split("")
+  let reverseArray = [];
+  for (i = string.length - 1; i >= 0; i--) {
+    reverseArray.push(string[i]);
+  }
+
+  return reverseArray.join('');
+}
+console.log(reverse("abcde"));
