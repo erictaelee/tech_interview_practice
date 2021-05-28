f69b7ae199745cb2571dd24e9b50daf30cc8eb65
Eric <lee.erictae@gmail.com>
2021-05-27 19:57:25 -0500

    visualize largest product problem

---
+++ largestProduct.js
@@ -0,0 +1,28 @@
+// Find the largest product of any two numbers within a given array.
+
+// Input: [5, -2, 1, -9, -7, 2, 6]
+// Output: 63 (-9 * -7)
+
+
+//write a function that finds the 2 largest integers and multiply
+//while loop
+//set largestNumber = array[0]
+//set secondLargestNumber = array[0]
+//set index = 0
+//if array[0] is greater than array[1] then largestNumber = array[0] 
+//if array[0] is greater than array[1] and less than largestNumber, then secondlargestNumber = array[0] 
+//i++
+//return largestNumber * secondLargestNumber
+
+
+
+//write a function that finds 2 smallest integers and multiply
+//compare the two products.
+//if largestIntegerProduct is greater than smallestInegerProduct than outputs the largestInegerProduct. 
+//else
+
+
+//return 
+
+
+
---
+++ palindrome.js
@@ -0,0 +1,18 @@
+// Given a string, write a function that returns true if it is a palindrome, and false if it is not. (A palindrome is a word that reads the same both forward and backward.)
+
+// Input: “racecar”
+// Output: true
+
+// Input: “baloney”
+// Output: false
+
+
+
+
+//write a function that reverses the string
+//create a palindrome function
+//if string == reverseString
+//if string == string.reverse()
+//return true
+//else false
+
