1824449ee4bae54f245ee05a819440a16b164058
Eric <lee.erictae@gmail.com>
2021-06-10 18:21:52 -0500

    try few logics in js

---
+++ arrayMesh1.js
@@ -0,0 +1,12 @@
+// Given two arrays of strings, return a new string that contains every combination of a string from the first array concatenated with a string from the second array.
+
+// Input: ["a", "b", "c"], ["d", "e", "f", "g"]
+// Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
+
+
+let array = ["a", "n"];
+let array2 = ["d"];
+let sum = [];
+
+sum = array + array2;
+console.log(sum);
