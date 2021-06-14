404714dbc474f04cc51af4c110704d8f7007f0e4
Eric <lee.erictae@gmail.com>
2021-06-14 18:51:51 -0500

    visualize palindrome exercise without creating a new databse

---
+++ palindrome.rb
@@ -0,0 +1,37 @@
+# Given a string, write a function that returns true if it is a palindrome, and false if it is not. (A palindrome is a word that reads the same both forward and backward.)
+
+# Input: “racecar”
+# Output: true
+
+# Input: “baloney”
+# Output: false
+
+
+racecar -> true
+repaper -> true
+hello -> false
+
+os =   rs =
+hello  olleh
+os = racecar
+rs = racecar 
+
+do it "in place"
+it means do this exercise without creating a new database
+
+
+racecar
+i 
+      j
+
+
+raceecar
+  i
+   j
+
+while i < j
+  i++
+  j--
+  return false if string[i] != string[j]
+
+return true
