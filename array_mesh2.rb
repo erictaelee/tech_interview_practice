9bf58451589db21d606992ff92375aa5cde954fe
Eric <lee.erictae@gmail.com>
2021-06-14 18:30:53 -0500

    start on array_mesh2 in ruby

---
+++ array_mesh2.rb
@@ -0,0 +1,20 @@
+# Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
+
+# Input: ["a", "b", "c", "d"]
+# Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
+
+
+(array[0] + array[1]) + (array[0] + array[2]) + ...
+(array[1] + array[0]) + 
+
+if array[i] = array[i2]
+  i += 1
+else
+  
+
+
+
+def array_mesh(array)
+
+
+end
