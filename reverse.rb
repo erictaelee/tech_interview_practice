39e9697d2267ba22d45a3008a18f8d74180dd9aa
Eric <lee.erictae@gmail.com>
2021-07-04 14:18:04 -0500

    solve reverse in ruby again

--- reverse.rb
+++ reverse.rb
@@ -3,16 +3,34 @@
 
 # string = "What's the weather like today"
 
+# def reversed_string(string)
+#   reversed_string = []
+#   split = string.split("")
+#   i = -1
+#   while i > ((string.length + 1) * -1)
+#     reversed_string << split[i]
+#     i -= 1
+#   end
+#   return reversed_string.join
+# end
+# p reversed_string("What's the weather like today")
+
+
+
+# Take the sentence "What's the weather like today?" and reverse it
+# Now, reverse the character order but keep the word order ("s'tahw eht rehtaew ...")
+
+# string = "What's the weather like today"
+
+
 def reversed_string(string)
-  reversed_string = []
-  split = string.split("")
-  i = -1
-  while i > ((string.length + 1) * -1)
-    reversed_string << split[i]
+  i = string.length - 1
+  result = ""
+  while i >= 0
+    result += string[i]
     i -= 1
   end
-  return reversed_string.join
+  return result
 end
+
 p reversed_string("What's the weather like today")
-
-
